f4f964cfe98c7372dd42ec795965f6477181f9b7
Andy Gup <agup2001@msn.com>
2016-01-13 12:56:59 -0700

    add license header to source

--- www/AdvancedGeolocation.js
+++ www/AdvancedGeolocation.js
@@ -1,4 +1,20 @@
-// https://cordova.apache.org/docs/en/latest/guide/hybrid/plugins/index.html
+/**
+ * @author Andy Gup
+ * Reference: https://cordova.apache.org/docs/en/latest/guide/hybrid/plugins/index.html
+ * 
+ * Copyright 2016 Esri
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.​
+ */
 var exec = cordova.require('cordova/exec');
 
 var AdvancedGeolocation = {
